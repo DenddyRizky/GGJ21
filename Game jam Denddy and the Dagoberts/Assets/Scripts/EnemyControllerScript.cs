a44f5cef8c91bcddc424a16260345ccf2d9cf271
NathanMisset <nathan_misset@live.nl>
2021-01-30 14:46:46 +0100

    Basic Enemy

--- Game jam Denddy and the Dagoberts/Assets/Scripts/EnemyControllerScript.cs
+++ Game jam Denddy and the Dagoberts/Assets/Scripts/EnemyControllerScript.cs
@@ -4,51 +4,34 @@ using UnityEngine;
 
 public class EnemyControllerScript : MonoBehaviour
 {
-    public List<string> libs;
-    Rigidbody2D body;
-    public int bodyPartCount;
+    public List<GameObject> libsTypes;
+    public List<GameObject> LibSlots;
+    
+    protected bool attackMode = false;
+    protected float attackRange;
 
-    public Vector3 startPosition;
-
-    public float runSpeed = 20.0f;
-    public float attackRange;
-    public bool attackMode = false;
-
-    public Vector2 waypoint;
-    Vector2 velocity;
-
-    public float mag;
+    public int maxLibCount;
 
     public float timer;
     public float checkTime;
 
-
-    public List<GameObject> partList;
-
-    public List<Sprite> spriteList;
-
     public List<Vector3> positionList;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        libs = new List<string>();
-        
-        body = GetComponent<Rigidbody2D>();
-
-        checkTime = 3;
-
-        for (int i = 0; i < bodyPartCount; i++)
+        positionList = new List<Vector3>();
+        for (int i = 0; i < maxLibCount; i++)
         {
-            partList.Add(new GameObject());
-            partList[i].transform.parent = gameObject.transform;
-            partList[i].transform.position = positionList[i];
-            SpriteRenderer renderer = partList[i].AddComponent<SpriteRenderer>();
-            renderer.sprite = spriteList[i];
+            positionList.Add(LibSlots[i].transform.position);
         }
-        startPosition = transform.position;
-        waypoint.x = Random.Range(startPosition.x - 3, startPosition.x + 2);
-        waypoint.y = Random.Range(startPosition.y - 3, startPosition.y + 2);
+        for (int i = 0; i < maxLibCount; i++)
+        {
+            LibSlots[i] = Instantiate(libsTypes[Random.Range(0, libsTypes.Count)] , gameObject.transform);
+            LibSlots[i].transform.position = positionList[i];
+        }
+        
 
     }
 
@@ -56,13 +39,6 @@ public class EnemyControllerScript : MonoBehaviour
     void Update()
     {
         timer += Time.deltaTime;
-    }
-
-    private void FixedUpdate()
-    {
-        Movement(transform.position);
-     
-        body.velocity = new Vector2(velocity.x , velocity.y) * runSpeed * Time.deltaTime;
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -73,36 +49,9 @@ public class EnemyControllerScript : MonoBehaviour
         }
     }
 
-    private void Movement(Vector3 playerLocation)
-    {
-        var target = waypoint;
+    
 
-        
+   
 
-        velocity = Vector2.Lerp(playerLocation, target, 1);
-
-        mag = target.magnitude - playerLocation.magnitude;
-        //mag = Mathf.Abs(mag);
-        if (mag < 0.5f && timer > checkTime)
-        {
-            if (transform.position.x > 0)
-            {
-                waypoint.x = Random.Range(startPosition.x - 5, startPosition.x - 2);
-            }
-            else if (transform.position.x <= 0)
-            {
-                waypoint.x = Random.Range(startPosition.x + 2, startPosition.x + 5);
-            }
-
-            if (transform.position.y > 0)
-            {
-                waypoint.y = Random.Range(startPosition.y - 5, startPosition.y - 2);
-            }
-            else if (transform.position.y <= 0)
-            {
-                waypoint.y = Random.Range(startPosition.y + 2, startPosition.y + 5);
-            }
-            timer = 0;
-        }
-    }
+    
 }
---
+++ Game jam Denddy and the Dagoberts/Assets/Scripts/TwoSlotEnemy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TwoSlotEnemy : EnemyControllerScript
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        libsTypes = new List<GameObject>();
+        LibSlots = new List<GameObject>();
+        maxLibCount = 2;
+
+        for (int i = 0; i < maxLibCount; i++)
+        {
+
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
