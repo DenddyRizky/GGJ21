3eeb6ef27a2a384146d164bd1befd59f3226c2c7
Sem Clous <sem.clous@gmail.com>
2021-01-30 01:27:05 +0100

    Working on Dash

--- Game jam Denddy and the Dagoberts/Assets/Scripts/Limbs/Legs/Rollerskates.cs
+++ Game jam Denddy and the Dagoberts/Assets/Scripts/Limbs/Legs/Rollerskates.cs
@@ -5,7 +5,14 @@ using UnityEngine;
 public class Rollerskates : Leg
 
 {
-    Rigidbody2D player;
+    public Rigidbody2D player;
+    private Vector3 target;
+    public float dashSpeed;
+    public float maxDashDist;
+
+    Vector3 mousePos;
+    Vector3 mouseDir;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -16,15 +23,38 @@ public class Rollerskates : Leg
     // Update is called once per frame
     void Update()
     {
+        float x = Input.GetAxisRaw("Horizontal");
+        float y = Input.GetAxisRaw("Vertical");
+
+        mouseDir = mousePos - transform.position;
+        
+        Debug.DrawRay(transform.position, mouseDir, Color.green);
+
         if (Input.GetButtonDown("Jump"))
-        {
-            Dash(player);
+        {   
+            Debug.Log("We goin");
+            Dash(x, y);
         }
 
     }
 
-    void Dash(Rigidbody2D player)
+    void Dash(float x, float y)
     {
-        
+        mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 moveDirection = (transform.position - mousePos);
+        moveDirection.z = 0;
+        moveDirection.Normalize();
+
+        //player.velocity = Vector2.zero;
+        //moveDirection = new Vector2(mousePos.x, mousePos.y);
+
+        player.AddForce(-moveDirection * dashSpeed);
+        Debug.Log((-moveDirection * dashSpeed));
+        //this.transform.position = Vector2.MoveTowards(transform.position, dir, maxDashDist);
+        //mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        //mousePos.z = 0;
+        //
+        //target = (mousePos - this.transform.position).normalized;
+        //player.AddForce(target * dashSpeed);
     }
 }
