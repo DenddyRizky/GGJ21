747b5c0f436c6e03da6a96f08f74ddb98cc9acff
Denddy <denddyrizky@gmail.com>
2021-01-30 12:58:37 +0100

    It done

---
+++ Game jam Denddy and the Dagoberts/Assets/Scripts/Limbs/Arms/TentacleSlap.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TentacleSlap : Arm
+{
+    public Transform attackPoint;
+    public float attackRange;
+    public LayerMask enemyLayers;
+    Vector3 worldMousePos;
+    Vector2 mouseDir;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        worldMousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mouseDir = (Vector2)(worldMousePos - transform.position);
+
+        mouseDir.Normalize();
+        attackPoint.position = (Vector2)transform.position + mouseDir;
+
+        if (Input.GetButtonDown("Fire1"))
+            Attack();
+    }
+
+    void Attack()
+    {
+        Debug.Log("ATTACK");
+        Collider2D[] hitEnemies = Physics2D.OverlapCircleAll(attackPoint.position, attackRange, enemyLayers);
+
+        foreach(Collider2D enemy in hitEnemies)
+        {
+            Debug.Log("HIT");
+        }
+    }
+    private void OnDrawGizmosSelected()
+    {
+        if (attackPoint == null)
+            return;
+
+        Gizmos.DrawWireSphere(attackPoint.position, attackRange);
+    }
+}
